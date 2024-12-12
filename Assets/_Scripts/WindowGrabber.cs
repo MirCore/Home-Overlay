78bd86db2b64617b85c9589975760bc09e3a12d5
MirCore <9919366+MirCore@users.noreply.github.com>
2024-12-12 11:23:39 +0100

    Recreated MainUI

---
+++ Assets/_Scripts/WindowGrabber.cs
@@ -0,0 +1,26 @@
+﻿using Structs;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR;
+
+public class WindowGrabber : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private Transform WindowTransform;
+    [SerializeField] private float ScaleFactor = 1;
+    
+    private void OnEnable()
+    {
+        IDragHandler foo = GetComponent<IDragHandler>();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (XRSettings.enabled)
+            return;
+        
+        Vector2 adjustedDelta = eventData.delta * ScaleFactor;
+
+        WindowTransform.localPosition += (Vector3)adjustedDelta;
+    }
+        
+}
